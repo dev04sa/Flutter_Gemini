22eca3fca8f9b88c40a0d7443fa0dd0c93aed71f
Dev <devsa2067@gmail.com>
2024-05-28 11:01:49 +0530

    project added

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:flutter_gemini_api/Const.dart';
+import 'package:flutter_gemini_api/pages/home_page.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  Gemini.init(apiKey: apiUrl);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
